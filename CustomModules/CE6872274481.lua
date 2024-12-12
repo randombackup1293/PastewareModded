5428aaed8d3689e11624d6bd91528115a562da15
randombackup1293 <themagicpiston145@gmail.com>
2024-12-12 18:13:38 -0500

    Update CE6872274481.lua

--- CustomModules/CE6872274481.lua
+++ CustomModules/CE6872274481.lua
@@ -5475,7 +5475,7 @@ run(function()
 							end
 						end
 
-						local speedValue = damagetick > tick() and SpeedValue.Value * 2.25 - 1 or SpeedValue.Value + getSpeed()
+						local speedValue = damagetick > tick() and SpeedValue.Value * 2.25 - 1 or SpeedValue.Value + (getSpeed() and (SpeedDamageBoost.Enabled and SpeedValue.Value * 1.1 or 0))
 						local speedVelocity = entityLibrary.character.Humanoid.MoveDirection * (SpeedMode.Value == "Normal" and SpeedValue.Value or 20)
 						entityLibrary.character.HumanoidRootPart.Velocity = antivoidvelo or Vector3.new(speedVelocity.X, entityLibrary.character.HumanoidRootPart.Velocity.Y, speedVelocity.Z)
 						if SpeedMode.Value ~= "Normal" then
